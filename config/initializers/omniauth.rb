a9959e258d2af5b490ee0f1fbc75d192b56341d9
Sharukh Hasan <hasan.sharukh@gmail.com>
2016-10-26 20:52:24 +0000

    adding omni authentication to initializer

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do |config|
+  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+end
