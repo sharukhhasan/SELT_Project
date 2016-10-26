c1c7d15a88a981c713ace6b453b15e672ea0fc2a
Sharukh Hasan <hasan.sharukh@gmail.com>
2016-10-26 21:01:02 +0000

    added route for spotify login and app id/secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do |config|
-  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+  provider :spotify, ENV['180509d7204c47ebbc519f8d8e2b5cb0'], ENV['e52ac4d2cb694b889716ecd7efa43faa'], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'playlist/new'
 
   get 'login/new'
+  
+  get '/auth/spotify/callback', to: 'users#spotify'
+  get 'users/show', to: 'users#show', as: 'playlist'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
