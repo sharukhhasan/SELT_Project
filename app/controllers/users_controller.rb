dc0e177115db48f1a81ecde78710545cf675986e
Sharukh Hasan <hasan.sharukh@gmail.com>
2016-10-26 20:40:15 +0000

    adding spotify authentication and playlist show to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  
+  def spotify
+    @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    render :index
+  end
 
   def show
-    @user = User.find(params[:id])
+    @playlist = RSpotify::Playlist.find(params[:user], params[:id])
   end
 
   def new
