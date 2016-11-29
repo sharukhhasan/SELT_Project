dbbdeaa069f829985a4bc1d82ac107c4dd5f98e0
Alexander Leinoff <alexander-leinoff@uiowa.edu>
2016-11-28 23:34:11 -0600

    Make search case-insensitive
    JQuery's contains() filter function is case sensitive. It seems more
    natural to have search be case insensitive. This solution was taken
    from an example on jquery's Sizzle wiki page:
    
    https://github.com/jquery/sizzle/wiki#-backwards-compatible-plugins-for-pseudos-with-arguments
    
    If anyone feels that this code is inappropriate code to use, please say so,
    and I'll try to work out a different solution (this works pretty well
    though). I fell like this usage is acceptable, as it is fully open source, and
    well cited.

--- app/assets/javascripts/playlist.js
+++ app/assets/javascripts/playlist.js
@@ -8,9 +8,32 @@ $(function () {
         filterDom(text);
     });
 
+    //The following 20 or so lines of code were taken directly from an example on github: https://github.com/jquery/sizzle/wiki#-backwards-compatible-plugins-for-pseudos-with-arguments
+    // An implementation of a case-insensitive contains
+    (function( $ ) {
+    function icontains( elem, text ) {
+        return (
+            elem.textContent ||
+            elem.innerText ||
+            $( elem ).text() ||
+            ""
+        ).toLowerCase().indexOf( (text || "").toLowerCase() ) > -1;
+    }
+    $.expr.pseudos.icontains = $.expr.createPseudo ?
+        $.expr.createPseudo(function( text ) {
+            return function( elem ) {
+                return icontains( elem, text );
+            };
+        }) :
+        function( elem, i, match ) {
+            return icontains( elem, match[3] );
+        };
+    })(jQuery);
+    //end github code
+
     var filterDom = function (text) { // filters playlist results based off of playlist name or track name
         $(".filterable").show(); // show all .filterable elements
-        var elements_not_containing_txt =  $(':not(.filterable:contains('+ text +'))'); // select jquery els with class .filterable not containing search txt
+        var elements_not_containing_txt =  $(':not(.filterable:icontains('+ text +'))'); // select jquery els with class .filterable not containing search txt
         var divs = elements_not_containing_txt.filter("div .filterable"); // of those elements grab only the divs
         divs.hide(); // hide those divs
     };
