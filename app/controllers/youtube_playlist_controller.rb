266fad67138025e97feea994897a484f85329743
Alexander Leinoff <alexander-leinoff@uiowa.edu>
2016-12-03 16:12:35 -0600

    WIP: TRying to get Yt client to create playlists and not being successful

--- app/controllers/youtube_playlist_controller.rb
+++ app/controllers/youtube_playlist_controller.rb
@@ -9,7 +9,10 @@ class YoutubePlaylistController < ApplicationController
   end
 
   def loggedin
-    @yt_account = Yt::Account.new access_token: session[:yt_token]
+    @yt_account = Yt::Account.new refresh_token: session[:yt_token]
+    Yt.configuration.client_id = '1095677510377-98krukjir1j472p4q3g7gb18oej274t2.apps.googleusercontent.com'
+    Yt.configuration.client_secret = '4UQ2tXPd9fIaUrOLuNdS9lE'
+    Yt.configuration.api_key = 'AIzaSyDl03kJVnB5b62LjXXnPgWKCoG_F4c5uyU'
     # account = session[:yt_token]
     byebug
   end
