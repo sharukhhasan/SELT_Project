0865c18a3af516e9405ab1c73508e5013d6d0687
Bill Rashid <mohammad-rashid@uiowa.edu>
2016-12-03 17:22:15 -0600

    created a playlist woooo

--- app/controllers/youtube_playlist_controller.rb
+++ app/controllers/youtube_playlist_controller.rb
@@ -1,3 +1,4 @@
+
 class YoutubePlaylistController < ApplicationController
   before_filter :set_current_user
 
@@ -9,11 +10,12 @@ class YoutubePlaylistController < ApplicationController
   end
 
   def loggedin
-    @yt_account = Yt::Account.new refresh_token: session[:yt_token]
+    @yt_account = Yt::Account.new access_token: session[:yt_token]
     Yt.configuration.client_id = '1095677510377-98krukjir1j472p4q3g7gb18oej274t2.apps.googleusercontent.com'
     Yt.configuration.client_secret = '4UQ2tXPd9fIaUrOLuNdS9lE'
     Yt.configuration.api_key = 'AIzaSyDl03kJVnB5b62LjXXnPgWKCoG_F4c5uyU'
-    # account = session[:yt_token]
+
+    @yt_account.create_playlist title: 'My favorites'
     byebug
   end
 end
