3fcaf9ea89542149904059fd47cdb37e49a59f00
Joe Hallman <gohawkeyes74@yahoo.com>
2016-12-06 23:23:13 +0000

    progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_session_token(session[:session_token])
-    
-    @user_birthday = session[:birthdate]
+    @user_display_name = session[:display_name]
+    @user_country = session[:country]
     @user_email = session[:email]
+    @user_images = session[:images]
   end
 end
