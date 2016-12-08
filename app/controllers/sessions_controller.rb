adf7106ab953e4b55a8bb1947f69bbebfc0a33c9
Alexander Leinoff <alexander-leinoff@uiowa.edu>
2016-12-08 11:58:47 -0600

    Fix some failing rspec tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     auth_spotify_user = RSpotify::User.new(auth_hash) # get a spotify user using omniauth token
     user = User.find_by_uid(auth_spotify_user.id)|| # lookup by id
               User.create_with_omniauth(auth_spotify_user) # if no user is found create one
-    session[:session_token] = user.session_token
+    session[:session_token] = user[:session_token]
     session[:country] = auth_spotify_user.country
     session[:followers] = auth_spotify_user.followers.total
     session[:display_name] = auth_spotify_user.display_name
